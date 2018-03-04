28990129ce40f224411fb8fc56d4873704345a8a
leybal <kravchuk018@gmail.com>
2018-03-04 17:09:28 +0200

    autorisation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./pages/home/home.component";
-import {LoginComponent} from "./pages/login/login.component";
-import {NotfoundComponent} from "./pages/notfound/notfound.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { UsersComponent } from "./pages/users/users.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { RegisterComponent } from './pages/register/register.component';
+import { AuthGuard } from './guards/index';
+import { NotfoundComponent } from "./pages/notfound/notfound.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: NotfoundComponent }
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,12 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AlertComponent } from './directives/index';
+import { AuthGuard } from './guards/index';
+import { AlertService, AuthenticationService, UserService } from './services/index';
+import { JwtInterceptor } from './helpers/index';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { UsersComponent } from './pages/users/users.component';
 
 
 @NgModule({
@@ -18,13 +28,28 @@ import { NotfoundComponent } from './pages/notfound/notfound.component';
     FooterComponent,
     HeaderComponent,
     LoginComponent,
-    NotfoundComponent
+    NotfoundComponent,
+    RegisterComponent,
+    UsersComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
