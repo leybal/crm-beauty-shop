ac03c686facb9eada37ea75eb5359f1cfd3d446f
im <marina.todorishina@mail.com>
2018-03-22 01:29:55 +0200

    Create SearchFilterPipe component

---
+++ client/src/app/pipes/search-filter.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'SearchFilterPipe',
+})
+export class SearchFilterPipe implements PipeTransform {
+  transform(value: any, input: string) {
+    if (input) {
+      input = input.toLowerCase();
+      return value.filter(function (el: any) {
+        return el.name.toLowerCase().indexOf(input) > -1;
+      });
+    }
+    return value;
+  }
+}
