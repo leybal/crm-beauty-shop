b15af867e3dc4fc58e2b986e1023c580f5919f95
im <marina.todorishina@mail.com>
2018-04-13 09:19:33 +0300

    Create carousel component

---
+++ client/src/app/shared/carousel/carousel.component.ts
@@ -0,0 +1,63 @@
+import {Component, OnInit} from '@angular/core';
+import {NgxCarousel} from 'ngx-carousel';
+import {ISubscription} from "rxjs/Subscription";
+
+import {environment} from '../../../environments/environment';
+import {User} from './../../models';
+import {UserService} from '../../services/index';
+
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent implements OnInit {
+  users: User[] = [];
+  avatarUrl = environment.avatarUrl;
+  public items: object[] = [];
+  public carouselConfig: NgxCarousel;
+  private subscription: ISubscription;
+
+  constructor(private userService: UserService) {
+  }
+
+  private loadAllUsers() {
+    this.subscription = this.userService.getAll()
+      .map(users => {
+        users.forEach(user => {
+          user.avatar = this.avatarUrl + user.avatar;
+          if (user.role === 'master') {
+            this.items.push(
+              {
+                name: user.name,
+                color: 'f1f1f1',
+                src: user.avatar,
+                id: user.id
+              }
+            );
+            console.log(user);
+          }
+        });
+        return users;
+      })
+      .subscribe(users => this.users = users);
+  }
+
+  ngOnInit() {
+
+    this.loadAllUsers();
+
+    this.carouselConfig = {
+      grid: {xs: 3, sm: 5, md: 5, lg: 5, all: 0},
+      slide: 1,
+      speed: 400,
+      interval: 10000,
+      point: {
+        visible: false
+      },
+      loop: true,
+      touch: true
+    };
+  }
+}
